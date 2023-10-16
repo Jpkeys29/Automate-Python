d9385edff659cf1b5e07e10f1cf3824fca54c0d0
Jp Keys <jpkeys@jps-macbook-pro.myfiosgateway.com>
2023-10-16 15:28:38 -0400

    NYC neighbors

--- table.py
+++ table.py
@@ -3,9 +3,13 @@ import html5lib
 
 # jorja = pd.read_html('https://en.wikipedia.org/wiki/Jorja_Smith#Discography')
 
-nyc = pd.read_html('https://en.wikipedia.org/wiki/New_York_City#Race_and_ethnicity')
-print(len(nyc))
-print(nyc[2])
+# nyc = pd.read_html('https://en.wikipedia.org/wiki/New_York_City#Race_and_ethnicity')
+# print(len(nyc))
+# print(nyc[3])
+
+neighborsNYC = pd.read_html('https://en.wikipedia.org/wiki/Neighborhoods_in_New_York_City')
+# print(len(neighborsNYC))
+print(neighborsNYC[0])
 
 
 # print(len(jorja))
