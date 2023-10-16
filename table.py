889a2831d6602bf9eb5a27df3b2af940de6ec044
Jp Keys <jpkeys@jps-macbook-pro.myfiosgateway.com>
2023-10-16 15:21:34 -0400

    table extraction

---
+++ table.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import html5lib
+
+# jorja = pd.read_html('https://en.wikipedia.org/wiki/Jorja_Smith#Discography')
+
+nyc = pd.read_html('https://en.wikipedia.org/wiki/New_York_City#Race_and_ethnicity')
+print(len(nyc))
+print(nyc[2])
+
+
+# print(len(jorja))
+# print(jorja[5])
+# print(jorja[1])
+# print(jorja[2])
+# print(jorja[6])
+
+
+
+
+
+             
